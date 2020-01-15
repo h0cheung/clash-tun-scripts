62c56107f19f990d85ac7b9ca3688917056dfb9f
Kr328 <39107975+Kr328@users.noreply.github.com>
2020-01-15 08:22:03 +0800

    fix cgroup init

--- setup-clash-cgroup.sh
+++ setup-clash-cgroup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+PROXY_BYPASS_USER="proxy"
+PROXY_BYPASS_CGROUP="0x16200000"
+PROXY_FWMARK="0x162"
+PROXY_ROUTE_TABLE="0x162"
+PROXY_DNS_SERVER="127.0.0.1:1053"
+PROXY_FORCE_NETADDR="198.18.0.0/16"
+PROXY_TUN_DEVICE_NAME="clash0"
+
 if [ -d "/sys/fs/cgroup/net_cls/bypass_proxy" ];then
 exit 0
 fi
