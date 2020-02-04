88053caa95e7b2d1b991207557c19263f4c7806d
Kr328 <39107975+Kr328@users.noreply.github.com>
2020-02-04 21:41:55 +0800

    fix bug

--- clean-clash-tun.sh
+++ clean-clash-tun.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+exec 1> /dev/null
+exec 2> /dev/null
+
 PROXY_BYPASS_USER="proxy"
 PROXY_BYPASS_CGROUP="0x16200000"
 PROXY_FWMARK="0x162"
 PROXY_ROUTE_TABLE="0x162"
-PROXY_DNS_SERVER="127.0.0.1:1053"
+PROXY_DNS_PORT="1053"
 PROXY_FORCE_NETADDR="198.18.0.0/16"
 PROXY_TUN_DEVICE_NAME="clash0"
+PROXY_TUN_ADDRESS="172.31.255.253/30"
 
 ip link set dev "$PROXY_TUN_DEVICE_NAME" down
 ip tuntap del "$PROXY_TUN_DEVICE_NAME" mode tun
@@ -18,13 +22,15 @@ iptables -t mangle -D OUTPUT -j CLASH
 iptables -t mangle -D PREROUTING -m set ! --match-set localnetwork dst -j MARK --set-mark "$PROXY_FWMARK"
 
 iptables -t nat -D OUTPUT -p udp --dport 53 -j CLASH_DNS
-iptables -t nat -D PREROUTING -p udp --dport 53 -j DNAT --to "$PROXY_DNS_SERVER"
+iptables -t nat -D PREROUTING -p udp --dport 53 -j REDIRECT --to-ports "$PROXY_DNS_PORT"
 
 iptables -t mangle -F CLASH
 iptables -t mangle -X CLASH
 
 iptables -t nat -F CLASH_DNS
 iptables -t nat -X CLASH_DNS
+
+iptables -t filter -D OUTPUT -d "$PROXY_TUN_ADDRESS" -j REJECT
 
 ipset destroy localnetwork
 
--- setup-clash-tun.sh
+++ setup-clash-tun.sh
@@ -4,11 +4,12 @@ PROXY_BYPASS_USER="proxy"
 PROXY_BYPASS_CGROUP="0x16200000"
 PROXY_FWMARK="0x162"
 PROXY_ROUTE_TABLE="0x162"
-PROXY_DNS_SERVER="127.0.0.1:1053"
+PROXY_DNS_PORT="1053"
 PROXY_FORCE_NETADDR="198.18.0.0/16"
 PROXY_TUN_DEVICE_NAME="clash0"
+PROXY_TUN_ADDRESS="172.31.255.253/30"
 
-/opt/script/clean-clash-tun.sh 2>&1 > /dev/null
+/opt/script/clean-clash-tun.sh
 
 ipset create localnetwork hash:net
 ipset add localnetwork 127.0.0.0/8
@@ -17,12 +18,12 @@ ipset add localnetwork 192.168.0.0/16
 ipset add localnetwork 224.0.0.0/4
 ipset add localnetwork 172.16.0.0/12 
 
-/opt/script/clash-setup-cgroup.sh
+/opt/script/setup-clash-cgroup.sh
 
 ip tuntap add "$PROXY_TUN_DEVICE_NAME" mode tun user $PROXY_BYPASS_USER
 ip link set "$PROXY_TUN_DEVICE_NAME" up
 
-ip address replace 10.40.0.1/24 dev "$PROXY_TUN_DEVICE_NAME"
+ip address replace "$PROXY_TUN_ADDRESS" dev "$PROXY_TUN_DEVICE_NAME"
 
 ip route replace default dev "$PROXY_TUN_DEVICE_NAME" table "$PROXY_ROUTE_TABLE"
 
@@ -41,11 +42,13 @@ iptables -t nat -N CLASH_DNS
 iptables -t nat -F CLASH_DNS 
 iptables -t nat -A CLASH_DNS -m owner --uid-owner "$PROXY_BYPASS_USER" -j RETURN
 iptables -t nat -A CLASH_DNS -m cgroup --cgroup "$PROXY_BYPASS_CGROUP" -j RETURN
-iptables -t nat -A CLASH_DNS -p udp -j DNAT --to "$PROXY_DNS_SERVER"
+iptables -t nat -A CLASH_DNS -p udp -j REDIRECT --to-ports "$PROXY_DNS_PORT"
 
 iptables -t mangle -I OUTPUT -j CLASH
 iptables -t mangle -I PREROUTING -m set ! --match-set localnetwork dst -j MARK --set-mark "$PROXY_FWMARK"
 
 iptables -t nat -I OUTPUT -p udp --dport 53 -j CLASH_DNS
-iptables -t nat -I PREROUTING -p udp --dport 53 -j DNAT --to "$PROXY_DNS_SERVER"
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to "$PROXY_DNS_PORT"
+
+iptables -t filter -I OUTPUT -d "$PROXY_TUN_ADDRESS" -j REJECT
 
