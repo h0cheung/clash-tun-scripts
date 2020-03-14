560568af5bc1f6a8f49cb651621c2e0d630883c3
h0cheung <36502569+h0cheung@users.noreply.github.com>
2020-03-14 17:26:28 +0800

    bypass udp port 6771 for BT Local Peer Discovery

--- setup-clash-tun.sh
+++ setup-clash-tun.sh
@@ -46,6 +46,7 @@ iptables -t mangle -A CLASH -d "$PROXY_FORCE_NETADDR" -j MARK --set-mark "$PROXY
 iptables -t mangle -A CLASH -m cgroup --cgroup "$PROXY_BYPASS_CGROUP" -j RETURN
 iptables -t mangle -A CLASH -m addrtype --dst-type BROADCAST -j RETURN
 iptables -t mangle -A CLASH -m set --match-set localnetwork dst -j RETURN
+iptables -t mangle -A CLASH -p udp --dport 6771 -j RETURN
 iptables -t mangle -A CLASH -j MARK --set-mark "$PROXY_FWMARK"
 
 ip6tables -t mangle -N CLASH6
